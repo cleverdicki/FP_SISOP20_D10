dd9b39999216c2e4212c385f2dc65cfbefa4bee9
Clever Dicki <48941105+cleverdicki@users.noreply.github.com>
2020-04-30 20:32:06 +0700

    Create cd.c

---
+++ cd.c
@@ -0,0 +1,20 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int main(int argc, char *argv[])
+{
+  if(argc < 2)
+  {
+    printf(1, "cd need arguments [path destination]\n");
+    exit();
+  }
+  
+  if(chdir(argv[1]) < 0)
+  {
+    printf(1, "Failed to change directory to *s!\n", argv[1]);
+  }
+  
+  exit();
+}
